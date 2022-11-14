6587dc27145ff59ca06c42062ea0cd5ba5024fac
NeoChoc <alexandra.groenert@web.de>
2022-11-14 13:51:30 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/03_Homepage/Homepage';
 import OnBoarding from './Pages/02_OnBoarding/OnBoarding';
 import SplashScreen from './Pages/01_SplashScreen/SplashScreen';
@@ -11,25 +11,22 @@ import Statistics from './Pages/07_Statistics/Statistics';
 import AddTransaction from './Pages/08_AddTransaction/AddTransaction';
 
 // styles
-
-import "./index.css";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SplashScreen />} />
-        <Route path='/onboarding' element={<OnBoarding />} />
-        <Route path='/home' element={<Homepage />} />
-        <Route path='/wallet' element={<Wallet />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/transaction/' element={<TransactionDetails />} />
-        <Route path='/statistics' element={<Statistics />} />
-        <Route path='/addtransaction' element={<AddTransaction />} />
-      </Routes>
-    </BrowserRouter>
-
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<SplashScreen />} />
+				<Route path="/onboarding" element={<OnBoarding />} />
+				<Route path="/home" element={<Homepage />} />
+				<Route path="/wallet" element={<Wallet />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/transaction/" element={<TransactionDetails />} />
+				<Route path="/statistics" element={<Statistics />} />
+				<Route path="/addtransaction" element={<AddTransaction />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
-
