993d2212812995f6772a7654e650cc3d7c64ce34
KatKlee <katklee2022@gmail.com>
2022-11-16 23:28:46 +0100

    worked on RegisterForm

--- src/Components/Forms/RegisterForm.jsx
+++ src/Components/Forms/RegisterForm.jsx
@@ -1,26 +1,55 @@
+import { useState } from 'react';
 import Button from '../Buttons/Button';
 import Upload from '../Upload';
 import styles from './RegisterForm.module.css';
 
 const RegisterForm = () => {
+
+	const [fullname, setFullname] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [file, setFile] = useState(null)
+
+	const registerUser = async () => {
+		const user = {
+			name: fullname,
+			email: email,
+			password: password
+		}
+
+		/* const response = await fetch('', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(user)
+		}) */
+
+		/* const data = await response.json()
+		console.log(data) */
+		console.log(fullname)
+		console.log(email)
+		console.log(password)
+	}
+
 	return (
 		<section className={styles.section}>
 			<article>
 				<div className={styles.inputContainer}>
 					<label className={styles.label} htmlFor="fullname">NAME</label>
-					<input className={styles.inputfield} type="text" name="fullname" id="fullname" placeholder='Full Name' />
+					<input onChange={(e) => setFullname(e.target.value)} className={styles.inputfield} type="text" name="fullname" id="fullname" value={fullname} placeholder='Full Name' />
 				</div>
 				<div className={styles.inputContainer}>
 					<label className={styles.label} htmlFor="email">EMAIL</label>
-					<input className={styles.inputfield} type="email" name="email" id="email" placeholder='E-Mail' />
+					<input onChange={(e) => setEmail(e.target.value)} className={styles.inputfield} type="email" name="email" id="email" value={email} placeholder='E-Mail' />
 				</div>
 				<div className={styles.inputContainer}>
 					<label className={styles.label} htmlFor="password">PASSWORD</label>
-					<input className={styles.inputfield} type="password" name="password" id="password" placeholder='Password' />
+					<input onChange={(e) => setPassword(e.target.value)} className={styles.inputfield} type="password" name="password" id="password" value={password} placeholder='Password' />
 				</div>
-				<Upload />
+				<Upload onchange={(e) => setFile(e.target.value)} value={file} />
 			</article>
-			<Button text={'Sign Up'} />
+			<Button onclick={registerUser} text={'Sign Up'} />
 		</section>
 	)
 }
