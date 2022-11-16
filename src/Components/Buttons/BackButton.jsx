c9687fd24f56c9d3a697b10607d830fda6ac967d
NeoChoc <alexandra.groenert@web.de>
2022-11-16 15:35:01 +0100

    test

--- src/Components/Buttons/BackButton.jsx
+++ src/Components/Buttons/BackButton.jsx
@@ -1,6 +1,12 @@
+import { Link } from 'react-router-dom';
 import styles from './BackButton.module.css';
 
 const BackButton = (props) => {
-	return <></>;
+	return (
+		<Link to={props.link}>
+			<img src={'../../Assets/Symbols/backbutton.png'} />
+		</Link>
+	);
 };
+
 export default BackButton;
