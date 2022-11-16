f1feae0a01fb70b4a87f6c4bac86ee062247078b
KatKlee <katklee2022@gmail.com>
2022-11-16 13:47:42 +0100

    added redirection to /onboarding after 2sec

--- src/Pages/01_SplashScreen/SplashScreen.jsx
+++ src/Pages/01_SplashScreen/SplashScreen.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './SplashScreen.module.css';
 
 const SplashScreen = () => {
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		setTimeout(() => {
+			navigate('/onboarding')
+		}, 2000)
+	}, [])
+
 	return (
 		<main className={styles.main}>
 			<h1 className={styles.headline}>mono</h1>
--- src/Pages/02_OnBoarding/OnBoarding.jsx
+++ src/Pages/02_OnBoarding/OnBoarding.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Login from '../../Components/Login';
 import OnboardingStart from '../../Components/OnboardingStart';
 import SignUp from '../../Components/SignUp';
