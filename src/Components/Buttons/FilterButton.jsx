db866e74e14f4d4a54754902b8102b92ed819042
NeoChoc <alexandra.groenert@web.de>
2022-11-18 12:08:51 +0100

    test

--- src/Components/Buttons/FilterButton.jsx
+++ src/Components/Buttons/FilterButton.jsx
@@ -2,11 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './FilterButton.module.css';
 
 const FilterButton = (props) => {
-	return (
-		<Link classList={styles.filterbutton} to={props.link}>
-			<img src={props.text} />
-		</Link>
-	);
+	return <Link classList={styles.filterbutton} to={props.link}></Link>;
 };
 
 export default FilterButton;
