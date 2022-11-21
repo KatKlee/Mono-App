5f9ad1b8f6f5df7c5f8c0fe80da4550b52f0a72c
KatKlee <katklee2022@gmail.com>
2022-11-21 20:18:19 +0100

    worked on AddTransaction and its componentsm, edited Upload

--- src/Pages/08_AddTransaction/AddTransaction.jsx
+++ src/Pages/08_AddTransaction/AddTransaction.jsx
@@ -1,12 +1,24 @@
+import BackButton from '../../Components/Buttons/BackButton';
+import LogoutButton from '../../Components/Buttons/LogoutButton';
+import AddForm from '../../Components/Forms/AddForm';
 import Navigation from '../../Components/Navigation/Navigation';
 import styles from './AddTransaction.module.css';
 
 const AddTransaction = () => {
 	return (
-		<>
-			<p>Eine neue Transaktion</p>
+		<main className={`${styles.main} ${styles.income}`}>
+
+			<section className={styles.headerSection}>
+				<BackButton />
+				<h3 className={styles.headline}>Add Income</h3>
+				<LogoutButton />
+			</section>
+
+			<AddForm />
+
+
 			<Navigation />
-		</>
+		</main>
 	);
 };
 
